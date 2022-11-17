1dbfccc462714ba71e2cbf34d1486dff1f483b93
Nick <nfears5798@gmail.com>
2022-11-17 15:51:03 -0500

    app controller

--- app/models/company.rb
+++ app/models/company.rb
@@ -4,25 +4,22 @@ class Company < ActiveRecord::Base
     has_many :investors, through: :stocks
 
     def company_profile
-        binding.pry
-        "#{self.company_name}, #{self.location}, #{self.sector}, #{self.assets_under_management}"
+        "#{self.company_name}, #{self.location}, #{self.sector}, $#{self.assets_under_management}"
     end
 
-    def currently_trading
-        binding.pry
-        self.stocks.where(is_actively_trading: true)
+    def self.add_company(company_name, location, sector, share_owned, assets_under_management)
+       self.find_or_create_by(company_name: company_name, location:location, sector:sector, share_owned:share_owned, assets_under_management:assets_under_management) 
     end
 
-    def add_company(company_name, location, sector, industry, exchange_short_name)
-       self.find_or_create_by(company_name: company_name, location:location, sector:sector, industry:industry, exchange_short_name:exchange_short_name) 
-       unless 
-        self.currently_trading == true
-       end
-    end
+    def self.delete_by_id(id)
+        company = Company.find(id)
+        company.destroy
+      end
 
+      def self.change_all_shares_and_assets(id, share_owned, assets_under_management)
+        Company.find_by(id:id).update(:share_owned=>share_owned, :assets_under_management=>assets_under_management)
+      end
 end
 
-#actively trading companies
-# find/create a company that doesnt exist in the database
 
 
--- app/models/controllers/application_controller.rb
+++ app/models/controllers/application_controller.rb
@@ -2,12 +2,57 @@ class ApplicationController < Sinatra::Base
     set :default_content_type, 'application/json'
     
     # Add your routes here
-    # get '/companies' do 
+    get '/companies' do 
+      companies = Company.all
+      companies.to_json
+    end
 
-    #   companies = Company.all
-  
-    #   companies.to_json
-    # end
+    get '/stocks' do 
+      stocks_all = Stock.all
+      stocks_all.to_json
+    end
+
+    get '/stocks/:id' do
+      stk = Stock.find(params[:id])
+      stk.to_json
+    end
+    
+
+    get '/companies/company_profile/:id' do 
+      profile = Company.company_profile.find(params[:id])
+      profile.to_json
+    end
+
+    post '/companies' do
+      new_company = Company.create(
+        company_name: params[:company_name],
+        location: params[:location],
+        sector: params[:sector],
+        share_owned: params[:share_owned],
+        assets_under_management: params[:assets_under_management]
+      )
+      new_company.to_json
+    end
+
+    get '/companies/:id' do
+      comp = Company.find(params[:id])
+      comp.to_json
+    end
+
+    patch '/companies/:id' do
+      company = Company.find(params[:id])
+      company.update(
+        share_owned: params[:share_owned],
+        assets_under_management: params[:assets_under_management]
+      )
+      company.to_json
+    end
+
+    delete '/companies/:id' do
+      d_comp = Company.find(params[:id])
+      d_comp.destroy
+      d_comp.to_json
+    end
 
     get '/stocks/by_price' do
       stock = Stock.by_price
@@ -28,11 +73,6 @@ class ApplicationController < Sinatra::Base
     get '/stocks/min_cap' do
       stock_min = Stock.min_cap
       stock_min.to_json
-    end
-
-    get '/stocks/a_to_z' do
-      stock_az = Stock.a_to_z
-      stock_az.to_json
     end
 
   end
